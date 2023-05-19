a0bbfd741e0ddd8dad84c9bbd2eb0811cb4aabaf
Phoenixfirewingz <lukethemodded@gmail.com>
2023-05-19 13:22:35 +0100

    Port To Linux
    changes to the script to package linux64 binary instead of mac os

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # README
 
-This is a repackaging fork of the Chromium Embedded Framework (CEF) binary distribution files for MacOS, found at https://cef-builds.spotifycdn.com/index.html, into a NuGet package.
+This is a repackaging fork of the Chromium Embedded Framework (CEF) binary distribution files for Linux 64x, found at https://cef-builds.spotifycdn.com/index.html, into a NuGet package.
--- make_cefredist.sh
+++ make_cefredist.sh
@@ -1,33 +1,26 @@
 TMP="tmp"
 OUTPUT="../package"
-
 if [ ! -d "$TMP" ]; then
     mkdir "$TMP"
 fi
-
 cd "$TMP"
-
 rm -rf "$OUTPUT"
 mkdir "$OUTPUT"
 mkdir "$OUTPUT/CEF"
 mkdir "$OUTPUT/CEF/Resources"
-
 CEFZIP="cef.tar.bz2"
 CEFBINARIES="cef_binaries"
 if [ ! -f "$CEFZIP" ]; then
     echo "downloading cef binaries"
-    curl -o "$CEFZIP" "https://cef-builds.spotifycdn.com/cef_binary_106.0.26%2Bge105400%2Bchromium-106.0.5249.91_macosx64_minimal.tar.bz2"
+    curl -o "$CEFZIP" "https://cef-builds.spotifycdn.com/cef_binary_106.0.26%2Bge105400%2Bchromium-106.0.5249.91_linux64_minimal.tar.bz2"
 fi
-
 if [ ! -d "$CEFBINARIES" ]; then
     echo "unzipping cef binaries"
     mkdir "$CEFBINARIES"
     tar -jxvf "$CEFZIP" -C "./$CEFBINARIES"
 fi
-
-CEFFRAMEWORK_DIR="$(find $CEFBINARIES -name "Release")/Chromium Embedded Framework.framework"
-
-cp "$CEFFRAMEWORK_DIR/Chromium Embedded Framework" "$OUTPUT/CEF/libcef.dylib"
-cp "$CEFFRAMEWORK_DIR/Libraries/"* "$OUTPUT/CEF/"
-cp "$CEFFRAMEWORK_DIR/Resources/"* "$OUTPUT/CEF/Resources/"
-cp "$CEFFRAMEWORK_DIR/Resources/en.lproj/"* "$OUTPUT/CEF/Resources/"
+echo "copying cef binaries"
+cp -Rv "${PWD}/$(find $CEFBINARIES -name "Release")/" "$OUTPUT/CEF/"
+cp -Rv "${PWD}/$(find $CEFBINARIES -name "Resources")/" "$OUTPUT/CEF/Resources/"
+cp -Rv "${PWD}/$(find $CEFBINARIES -name "Resources")/locales/" "$OUTPUT/CEF/Resources/locals/"
+cd ..
