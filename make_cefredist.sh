5d0bb0441f5364db8d2fac7d996d13f23ce7659d
Phoenixfirewingz <lukethemodded@gmail.com>
2023-05-22 10:16:31 +0100

    Fix Make Script
    fix to strip useless info from Libs

--- make_cefredist.sh
+++ make_cefredist.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 TMP="tmp"
 OUTPUT="../package"
 if [ ! -d "$TMP" ]; then
     mkdir "$TMP"
 fi
-cd "$TMP"
+cd "$TMP" || exit 1
 rm -rf "$OUTPUT"
 mkdir "$OUTPUT"
 mkdir "$OUTPUT/CEF"
@@ -20,6 +21,14 @@ if [ ! -d "$CEFBINARIES" ]; then
     tar -jxvf "$CEFZIP" -C "./$CEFBINARIES"
 fi
 echo "copying cef binaries"
-cp -v "${PWD}/$(find $CEFBINARIES -name "Release")/libcef.so" "$OUTPUT/CEF/libcef.so"
+cp -va "${PWD}/$(find $CEFBINARIES -name "Release")/." "$OUTPUT/CEF/"
+cd .. || exit 1
+echo "stripping cef binaries"
+strip -v -s "package/CEF/libcef.so"
+strip -v -s "package/CEF/libEGL.so"
+strip -v -s "package/CEF/libGLESv2.so"
+strip -v -s "package/CEF/libvk_swiftshader.so"
+strip -v -s "package/CEF/libvulkan.so.1"
+cd "$TMP" || exit 1
 cp -Rv "${PWD}/$(find $CEFBINARIES -name "Resources")/" "$OUTPUT/CEF/"
 cd ..
